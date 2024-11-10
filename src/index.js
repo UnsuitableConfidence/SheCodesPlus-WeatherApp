cd902a677dd425b3c9489158f318dedeab79d106
UnsuitableConfidence <161158334+UnsuitableConfidence@users.noreply.github.com>
2024-11-10 13:52:58 +0000

    Added live weather info

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 function refreshData(response) {
+  //Current temperature
   let currentTemperatureElement = document.querySelector(
     "#current-temperature"
   );
   console.log(response.data.temperature.current);
   let currentTemperature = Math.round(response.data.temperature.current);
   currentTemperatureElement.innerHTML = currentTemperature;
+  //Current weather
+  let currentWeatherElement = document.querySelector("#current-weather");
+  console.log(response.data.condition.description);
+  let currentWeather = response.data.condition.description;
+  currentWeatherElement.innerHTML = currentWeather;
+  //MoreInfo
+  //Humidity/Windspeed/Pressure/RealFeel
+  let realfeelElement = document.querySelector("#realfeel");
+  let realfeel = Math.round(response.data.temperature.feels_like);
+  realfeelElement.innerHTML = realfeel;
+
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  humidityElement.innerHTML = humidity;
+
+  let windspeedElement = document.querySelector("#windspeed");
+  let windspeed = response.data.wind.speed;
+  windspeedElement.innerHTML = windspeed;
+
+  let pressureElement = document.querySelector("#pressure");
+  let pressure = response.data.temperature.pressure;
+  pressureElement.innerHTML = pressure;
+  //Add units to the HTML
 }
 
 function searchCity(city) {
@@ -12,6 +36,7 @@ function searchCity(city) {
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   //update so can select unit
   axios.get(apiURL).then(refreshData);
+  console.log(apiURL);
 }
 
 function searchSubmit(event) {
